d1210220ef1a2661113b4d6a0219a01839c63ae7
wcrvieira <wagner.vieira01@cps.sp.gov.br>
2026-05-13 21:30:58 -0300

    Inserindo registros

--- cinema/cinema.sql
+++ cinema/cinema.sql
@@ -1,4 +1,8 @@
 -- Linguagem SQL padrão ANSI-92 ou SQL-92
+
+-- Exclusão do banco de dados, se existir
+DROP DATABASE IF EXISTS cinema;
+
 -- Criação do banco de dados cinema
 CREATE DATABASE IF NOT EXISTS cinema;
 
@@ -20,6 +24,12 @@ CREATE TABLE IF NOT EXISTS clientes
   estado CHAR(2)
 );
 
+-- Inserção de registros na tabela de clientes
+INSERT INTO clientes VALUES
+(53144243124, 'Leandro Benedito da Cruz', 'M', '1973-04-23', '(31) 98452-2893', 'leandro.cruz@hottmail.com', 'Rua Beco Esperança, 152', 'Vila Renascer', 'Contagem', 'MG'),
+(70647543800, 'Rayssa Elisa da Cunha', 'F', '1998-04-22', '(86) 98417-7026', 'rayssacunha@tecsys.com.br', 'Quadra 27, 714', 'Jardim São Vicente', 'Parnaíba', 'PI'),
+(09964328842, 'Rafaela Raquel Viana', 'F', '2002-03-02', '(83) 98698-7849', 'rafaela_viana02@sandvik.com', 'Rua Vitório Souza, 875', 'Res. Mangabeira', 'João Pessoa', 'PB');
+
 -- Criação da tabela categorias
 CREATE TABLE IF NOT EXISTS categorias
 ( id_categoria INT(6) PRIMARY KEY AUTO_INCREMENT,
@@ -28,17 +38,29 @@ CREATE TABLE IF NOT EXISTS categorias
   data_cadastro TIMESTAMP -- traz a data e hora do servidor
 );
 
+-- Inserção de registros na tabela de categorias
+INSERT INTO categorias VALUES
+(0, 'Aventura e ação', 'Focada em jornadas, perseguições e lutas', CURRENT_TIMESTAMP),
+(0, 'Ficção científica', 'Histórias baseadas em ciência especulativa', CURRENT_TIMESTAMP),
+(0, 'Terror e suspense', 'Criado para provocar medo, mistério e choque', CURRENT_TIMESTAMP);
+
 -- Criação da tabela filmes
 CREATE TABLE IF NOT EXISTS filmes
 ( codigo INT PRIMARY KEY AUTO_INCREMENT,
   titulo VARCHAR(30) NOT NULL,
-  sinopse VARCHAR(50),
-  elenco VARCHAR(100),
+  sinopse VARCHAR(150),
+  elenco VARCHAR(70),
   data_lancamento DATE,
   id_filme INT(6),
   -- Chave estrangeira conectando o filme com a categoria
   FOREIGN KEY (id_filme) REFERENCES categorias (id_categoria)
 );
+
+-- Inserção de registros na tabela de filmes
+INSERT INTO filmes VALUES
+(0, 'O justiceiro: uma última morte', 'Enquanto Frank Castle busca um significado além da vingança, uma força inesperada o arrasta de volta para a luta.','Jon Berthal, Deborah Ann Woll, Jason R. Moore', '2026-05-12', 1),
+(0, 'Mortal Kombat 2', 'Os campeões são colocados uns contra os outros numa sangrenta e derradeira batalha, sem regras ou limites.', 'Karl Urban, Adeline Rudolph, Jessica McNamee', '2026-05-07', 1),
+(0, 'Faces da Morte', 'Uma moderadora de uma plataforma de compartilhamento de vídeos na internet se depara com uma possível rede de filmes snuff.', 'Barbie Ferreira, Dacre Montgomery, Josie Totah', '2026-04-10', 3);
 
 -- Criação da tabela salas
 CREATE TABLE IF NOT EXISTS salas
